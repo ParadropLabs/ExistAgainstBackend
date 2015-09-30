01f2321228c67bdebea40d200bbe711c18b3b7cf
Mickey Barboi <damouse007@gmail.com>
2015-09-30 16:31:08 -0500

    pod contents

--- FabAgainstBackend/Room.swift
+++ FabAgainstBackend/Room.swift
@@ -6,24 +6,207 @@
 //  Copyright © 2015 paradrop. All rights reserved.
 //
 
-// A collection of players in one place.
+/*
+    A room is a  game between a limited set of players. Creates a random string
+    which it appends to /room for its endpoints.
+
+    Gameplay proceeds in rounds which consist of phases.
+
+    Publishes on:
+    /room/round/picking
+    /room/round/choosing
+    /room/round/scoring
+    /room/round/cancel
+    /room/round/tick
+
+    /room/joined
+    /room/left
+    /room/closed
+
+    /room/play/picked
+
+    Registers:
+    /room/current
+    /room/play/pick
+
+*/
 
 import Foundation
 
+enum State {
+    case Empty, Picking, Choosing, Scoring
+}
+
 
 class Room {
-    var name: String
-    var closing: (Room) -> ()
+    var name = randomStringWithLength(6)
+    var players: [Player] = []
+    var chooser: Player?
+    var session: Session
+    var state: State = .Empty
+    var timer: AnyObject?
     
-    init(name: String, onClose: (Room) -> ()) {
-        name = name
-        closing = onClose
+    var questions: [[String: AnyObject]]
+    var answers: [[String: AnyObject]]
+    
+    
+    init(session s: Session, questions q: [[String: AnyObject]], answers a: [[String: AnyObject]]) {
+        session = s
+        questions = q
+        answers = a
+        
+        //subscribe playerLeft
+    }
+    
+    
+    // MARK: Player Changes
+    func addPlayer(id: String) -> [Int] {
+        // Called from main session when players arrive
+        // return room information to the player
+        // return cards to the player
+        
+        // Assumes no duplicates, obviously
+        players.append(Player(pdid: id))
+        
+        //publish join
+        
+        // Check if we have enough people to start playing
+        if players.count > 1 {
+            startPicking()
+        }
+        
+        // draw cards for the player
+        var hand: [Int] = []
+        for _ in 0...HAND_SIZE {
+            hand.append(randomElement(answers)["id"] as! Int)
+        }
+        
+        return hand
+    }
+    
+    func playerLeft(id: String) {
+        // get player that left
+        // If chooser, cancel and reassign
+        // Publish leave
+        
+        // Make sure we have enough players to play
+        if players.count < 2 {
+            //stop timer
+            //publish cancel
+        }
+    }
+    
+    
+    // MARK: Picking
+    func startPicking() {
+        state = .Picking
+        
+        // reset picked state
+        _ = players.map { $0.pick = nil }
+        
+        // draw cards for players
+        
+        // choose next player in round robin
+        // chooser = chooser == nil ? players[0] : players[players.indexOf(chooser!) + 1 % players.count()]
+        
+        // session.publish("/room/round/", newChooser)
+        // Start timer
+    }
+    
+    func pick(id: String, card: Int) {
+        // Ensure state, throw exception
+        
+        // get the player
+        let player = getPlayer(id)
+        
+        if player.pick != nil {
+            print("Player has already picked a card.")
+            return
+        }
+        
+        player.pick = card
+        
+        //remove card from player
+        
+        // publish picked
+    }
+    
+    // MARK: Choosing
+    func startChoosing() {
+        state = .Choosing
+        
+        // publish picks
+        // start timer
+    }
+    
+    func chose(id: String) {
+        // stop timer
+        startScoring(getPlayer(id))
+    }
+    
+    
+    //MARK: Scoring
+    func startScoring(player: Player?) {
+        state = .Scoring
+        
+        if let p = player {
+            p.score += 1
+        }
+        
+        // if called with nil, no one chose. Else publish the winner.
+        // publish scoring
+        
+        // start timer
+    }
+    
+    
+    // MARK: Utils
+    func getPlayer(id: String) -> Player {
+        return players.filter({$0.id == id})[0]
+    }
+    
+    func drawCards(number: Int) -> [Int] {
+        // draws a number of cards for the player. Tracks duplicates (?)
+        var ret: [Int] = []
+        
+        let TEMPCARDS = [1, 2, 3]
+        
+        for _ in 0...number {
+            ret.append(randomElement(TEMPCARDS))
+        }
+        
+        return ret
     }
 }
 
 
-// MARK: Player
 class Player {
     var id: String
-    var score: Int
+    var score = 0
+    var pick: Int?
+    
+    init(pdid: String) {
+        id = pdid
+    }
+}
+
+
+// Utility function to generate random strings
+func randomStringWithLength (len : Int) -> NSString {
+    
+    let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    let randomString : NSMutableString = NSMutableString(capacity: len)
+    
+    for (var i=0; i < len; i++){
+        let length = UInt32 (letters.length)
+        let rand = arc4random_uniform(length)
+        randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
+    }
+    
+    return randomString
+}
+
+func randomElement<T>(arr: [T]) -> T {
+    // returns a random element from an array
+    return arr[Int(arc4random_uniform(UInt32(arr.count)))]
 }
--- FabAgainstBackend/Session.swift
+++ FabAgainstBackend/Session.swift
@@ -9,14 +9,45 @@
 import Foundation
 import Riffle
 
+let ROOM_CAP = 6
+let HAND_SIZE = 6
 
 class Session: RiffleSession {
+    var rooms: [Room] = []
+    
+    // Maybe move this...
+    var cardsQuestion13: [[String: AnyObject]] = []
+    var cardsAnswer13: [[String: AnyObject]] = []
+    var cardsQuestion21: [[String: AnyObject]] = []
+    var cardsAnswer21: [[String: AnyObject]] = []
+    
     override func onJoin() {
-        register("pd.demo.cardsagainst/getRoom", getRoom)
+        // Load cards
+        cardsQuestion13 = loadCards("q13")
+        cardsAnswer13 = loadCards("a13")
+        cardsQuestion21 = loadCards("q21")
+        cardsAnswer21 = loadCards("a21")
+        
+        register("pd.demo.cardsagainst/play", getRoom)
     }
     
-    func getRoom() {
-        // Assign the given agent to a room
-        print("Called")
+    func getRoom(player: String) -> [Int] {
+        // Assign the player to a room. Returns cards for the player
+        let emptyRooms = rooms.filter { $0.players.count <= ROOM_CAP }
+        var room: Room
+        
+        if emptyRooms.count == 0 {
+            room = Room(session: self, questions: cardsQuestion13, answers: cardsAnswer13)
+            rooms.append(room)
+        } else {
+            room = emptyRooms[Int(arc4random_uniform(UInt32(emptyRooms.count)))]
+        }
+        
+        return room.addPlayer(player)
     }
+}
+
+func loadCards(name: String) -> [[String: AnyObject]] {
+    let jsonPath = NSBundle.mainBundle().pathForResource(name, ofType: "json")
+    return try! NSJSONSerialization.JSONObjectWithData(NSData(contentsOfFile: jsonPath!)!, options: NSJSONReadingOptions.AllowFragments) as! [[String: AnyObject]]
 }
