643772c389a31a0ac40e0252d5e7d36b1027dce3
Mickey Barboi <damouse007@gmail.com>
2015-11-02 14:01:16 -0600

    updated shared code with rifflemodel changes

--- FabAgainstBackend/Shared.swift
+++ FabAgainstBackend/Shared.swift
@@ -12,13 +12,12 @@ import Riffle
 
 class Player: RiffleModel {
     var id = -1
-    
     var domain = ""
     var score = 0
-    
     var chooser = false
     var hand: [Card] = []
     var pick: Card?
+    
     
     override static func JSONKeyPathsByPropertyKey() -> [NSObject : AnyObject]!  {
         // By default, do not pass along nested model objects
@@ -43,6 +42,44 @@ func ==(lhs: Player, rhs: Player) -> Bool {
     return lhs.domain == rhs.domain
 }
 
+
+class Deck {
+    var questions: [Card] = []
+    var answers: [Card] = []
+    
+    init(questionPath: String, answerPath: String) {
+        let load = { (name: String) -> [Card] in
+            let jsonPath = NSBundle.mainBundle().pathForResource(name, ofType: "json")
+            let x = try! NSJSONSerialization.JSONObjectWithData(NSData(contentsOfFile: jsonPath!)!, options: NSJSONReadingOptions.AllowFragments) as! [[String: AnyObject]]
+
+            return x.map({ (json: [String: AnyObject]) -> Card in
+                let card = Card()
+                card.id = json["id"] as! Int
+                card.text = json["text"] as! String
+                return card
+            })
+        }
+        
+        questions = load(questionPath)
+        answers = load(answerPath)
+    }
+    
+    func drawCards(var cards: [Card], number: Int, remove: Bool = true) -> [Card] {
+        var ret: [Card] = []
+        
+        for _ in 0...number {
+            ret.append(randomElement(&cards, remove: remove))
+        }
+        
+        return ret
+    }
+    
+    func reshuffleCards(inout target: [Card], cards: [Card]) {
+        // "Realease" the cards formerly in play by shuffling them back into the deck 
+        target.appendContentsOf(cards)
+    }
+}
+
 func getPlayer(players: [Player], domain: String) -> Player {
     return players.filter({$0.domain == domain})[0]
 }
@@ -62,6 +99,7 @@ func randomStringWithLength (len : Int) -> String {
 }
 
 func randomElement<T>(inout arr: [T], remove: Bool = false) -> T {
+    // fails if the array is empty
     let i = Int(arc4random_uniform(UInt32(arr.count)))
     let o = arr[i]
     
